25d12138252e6d5a013507c1e92f4c7e486a350b
viking <wiktor.przadka.1@gmail.com>
2020-11-09 10:19:52 +0100

    use laplace transform to make simulation

--- mass-spring-damper/model.py
+++ mass-spring-damper/model.py
@@ -1,9 +1,25 @@
-# import scipy.integrate as integrate
+from sympy import inverse_laplace_transform, diff
+from sympy.abc import s, t
 import matplotlib
 import matplotlib.pyplot as plt
 
 matplotlib.use('Qt5Agg')
 
+
+def euler_method(derivatives: list, step: float):
+    for idx in reversed(range(len(derivatives) - 1)):
+        derivatives[idx] += derivatives[idx + 1] * step
+
+
+def transfer_function(mass: float, damping: float, elasticity: float):
+    position_func = inverse_laplace_transform(1/(mass * s**2 + damping * s + elasticity), s, t)
+    velocity_func = diff(position_func)
+    accelerate_func = diff(velocity_func)
+    return position_func, velocity_func, accelerate_func
+
+
+# if __name__ == '__main__':
+#     print(transfer_function(1, 0.3, 1))
 
 if __name__ == '__main__':
 
@@ -22,20 +38,26 @@ if __name__ == '__main__':
     time_series = []
     force_series = []
 
+    func = transfer_function(mass, damping, elasticity)
+    for f in func:
+        print(f)
+
     time = 0
     while time < total_time:
-        pos_derivatives[2] = (force_func(time) - damping * pos_derivatives[1] - elasticity * pos_derivatives[0]) / mass
-        for i in (1, 0):
-            pos_derivatives[i] += pos_derivatives[i + 1] * step
-
+        # pos_derivatives[2] = (force_func(time) - damping * pos_derivatives[1] - elasticity * pos_derivatives[0]) / mass
+        # euler_method(pos_derivatives, step)
+        #
         force_series.append(force_func(time))
-
         for idx, curr_series in enumerate(series.values()):
-            curr_series.append(pos_derivatives[idx])
+            curr_series.append(func[idx].subs([(t, time)]))
+        #     curr_series.append(pos_derivatives[idx])
+
         time_series.append(time)
         time += step
 
-    # time_series = [i for i in range(total_time, 0, step)]
+    for curr_series in series.values():
+        curr_series[0] = 0
+
     fig, axis = plt.subplots(4, 1)
 
     for idx, (series_name, series_values) in enumerate(
