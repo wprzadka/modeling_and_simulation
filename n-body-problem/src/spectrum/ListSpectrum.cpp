e772cb7c02f2e7d2780f7c5ab9e6c62fdb45ff40
viking <wiktor.przadka.1@gmail.com>
2020-11-16 09:08:29 +0100

    make list spectrum draw vertices only at visible distances

--- n-body-problem/src/spectrum/ListSpectrum.cpp
+++ n-body-problem/src/spectrum/ListSpectrum.cpp
@@ -2,6 +2,7 @@
 // Created by viking on 10.11.2020.
 //
 
+#include <cmath>
 #include "../../includes/spectrum/ListSpectrum.h"
 
 ListSpectrum::ListSpectrum(): Spectrum(), maxSize(200), points(std::make_unique<sf::Vertex[]>(200)) {
@@ -13,6 +14,10 @@ void ListSpectrum::draw(sf::RenderWindow &window) {
 }
 
 void ListSpectrum::addPosition(sf::Vector2f position) {
+    auto distanceFromLastVec = points[0].position - position;
+    if(sqrt(pow(distanceFromLastVec.x, 2) + pow(distanceFromLastVec.y, 2)) < 1){
+        return;
+    }
     for(int i = size- 1; i > 0; --i){
         points[i] = points[i - 1];
         if(points[i].color.a > 0)
